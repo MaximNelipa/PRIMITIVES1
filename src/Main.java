ba1525275c7baeac3a7a8a9b3d36358f5f9a01df
MAX <max_nelipa@mail.ru>
2023-03-11 23:51:44 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 50000;
+        int bonusMile = 20;
+        int bonus = ticketPrice / bonusMile;
+
+        System.out.println("Итоговый бонус: " + bonus);
+
+
+
+
+
+    }
+}
